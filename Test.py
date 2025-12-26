ba679af47f24e118344ccd9917b4425c69fdd780
paarthrajbala30-cmd <paarth.rajbala30@gmail.com>
2025-12-26 12:05:10 +0000

    update

--- Test.py
+++ Test.py
@@ -7,12 +7,13 @@ for x in values:
 print(10 in[1, 2, 3, 4])
 
 #program to print your name nine times using for loop with range() function.
-name = input("Paarth")
-for i in range(9):
+name = input("Enter your name : ")
+for i in range(3):
     print(name)
-
+'''
 for i in range(5) :
     if i == 2:
         continue
     print(i)
     print('Out of loop and 2 is skiped')
+    '''
